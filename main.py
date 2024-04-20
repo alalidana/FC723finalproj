84eb6294e248eb72897c7cdc68afec9603209890
alalidana <148446876+alalidana@users.noreply.github.com>
2024-04-20 18:26:24 +0100

    adding if statement to connect the users choice to the right function

--- main.py
+++ main.py
@@ -12,6 +12,22 @@ def main_menu():
         #ask the user to make a choice from the menu options
         choice= input("Enter your choice (1-5): ")
 
+        #if statement to connect the users choice to the right function
+        if choice == '1':
+            check_seat_availability()
+        elif choice == '2':
+            book_seat()
+        elif choice == '3':
+            free_seat()
+        elif choice == '4':
+            show_booking_state()
+        elif choice == '5':
+            exit_program()
+            # Exit the loop to terminate the program
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
 #define function to check the avaiability of the seats
 def check_seat_availability():
     pass
