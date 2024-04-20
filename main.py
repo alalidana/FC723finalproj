a6a3e35e28e4930ffd03b193ecf19f0bbd795570
alalidana <148446876+alalidana@users.noreply.github.com>
2024-04-20 06:58:19 +0100

    creating menu's user interface

---
+++ main.py
@@ -0,0 +1,9 @@
+def main_menu():
+    while True:
+        print("\nWelcome to Apache Airlines Seat Booking System")
+        print("1. Check seat availability")
+        print("2. Book a seat")
+        print("3. Free a seat")
+        print("4. Show booking state")
+        print("5. Exit program")
+        choice= input("Enter your choice (1-5): ")
